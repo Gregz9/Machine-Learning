461fe6851636677b11531de33c516ab826bcad91
Gregor Kajda <gregor.kajda@mindshareworld.com>
2022-09-27 18:29:53 +0200

    Functional OSL for fitting of a polynomial upto fifth order to Franke's function

--- FYS_STK4155/franke_function.py
+++ FYS_STK4155/franke_function.py
@@ -4,13 +4,17 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from random import random, seed
+from sklearn.preprocessing import StandardScaler
 
-fig = plt.figure() 
+
+fig = plt.figure()
+fig2 = plt.figure() 
 ax = fig.add_subplot(projection='3d')
+ax2 = fig2.add_subplot(projection='3d')
 
 # Making data ingestion to the function
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05) 
+x = np.arange(0, 1, 0.025)
+y = np.arange(0, 1, 0.025) 
 x, y = np.meshgrid(x,y)
 
 def FrankeFunction(x, y, noise=False):
@@ -36,7 +40,7 @@ def generate_design_matrix(x, y, order):
             X = np.column_stack((X, x**(i+1), y**(i+1)))
         else: 
             X = np.column_stack((X, x**(i+1), y**(i+1), (x**i)*(y**i)))
-    return X 
+    return np.array(X) 
 
 def compute_optimal_parameters(X, y): 
     # This method uses SVD to compote the optimal parameters beta for OSL.
@@ -48,10 +52,6 @@ def compute_optimal_parameters(X, y):
     for i in range(len(S)):
         D[i,i] = S[i]
     
-    print('singular-value matrices')
-    print(np.linalg.inv(D))
-    print(np.linalg.inv(np.diag(S)))
-
     beta = (VT.T)@(np.linalg.inv(np.diag(S)))@(U.T)@(y.ravel())
     return beta
 
@@ -61,41 +61,68 @@ def optimal_parameters_inv(X, y):
     #beta = beta.T
 
     beta = beta@(y.ravel())
-    print(beta)
+    return beta
 
 
 def perform_regression(X, beta): 
 
     franke_pred = np.array(())
     for i in range(X.shape[0]):
-        for j in range(X.shape[1]):
-            
-            franke_pred = np.append(franke_pred, np.sum(beta*X[i,:]))
+
+        franke_pred = np.append(franke_pred, np.sum(X[i]*beta))
+
+    return franke_pred
     
-    print(franke_pred.shape)
+def perform_manual_regression(x, y, beta): 
+    pred = beta[0] + beta[1]*x + beta[2]*y + beta[3]*x**2 + beta[4]*y**2 + beta[5]*x*y
+    return pred
 
+X2 = generate_design_matrix(x, y, 5)
+"""print('\n')
+x2 = x.ravel() 
+y2 = y.ravel()
 
-    
+X3 = np.stack((np.ones(len(x2)), x2, y2, x2**2, y2**2, x2*y2), axis=-1)
+print(X3)
 
-
-X2 = generate_design_matrix(x, y, 3)
-print(X2.shape())
-beta_SVD = compute_optimal_parameters(X2, y)
-beta_INV = optimal_parameters_inv(X2, y)
-perform_regression(X2, beta_SVD)
+print(np.array_equal(X2, X3))
+"""
 
 z = FrankeFunction(x, y) 
 
+beta_SVD = compute_optimal_parameters(X2, z)
+beta_INV = optimal_parameters_inv(X2, z)
+
+print(beta_INV)
+print(beta_SVD)
+
+preds_man = perform_manual_regression(x, y, beta_SVD)
+preds = perform_regression(X2, beta_SVD)
+preds = preds.reshape(len(x), len(y))
+
+"""
+x_and_y = np.hstack((x.ravel().reshape(x.ravel().shape[0],1), y.ravel().reshape(y.ravel().shape[0],1)))
+scaler = StandardScaler()
+scaler.fit(x_and_y)
+
+X_scaled = scaler.transform(x_and_y)
+
+X = generate_design_matrix(X_scaled.T[0], X_scaled.T[0], 2)
+beta = compute_optimal_parameters(X, z)
+"""
 
 # Plot the surface of the function
 surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
+surf2 = ax2.plot_surface(x, y, preds, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 # Customization of z-axis
 ax.set_zlim(-0.10, 1.40)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
+ax2.set_zlim(-0.10, 1.40)
+ax2.zaxis.set_major_locator(LinearLocator(10))
+ax2.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 # Add a color bar which maps values to colors 
 fig.colorbar(surf, shrink=0.5, aspect=5)
-
+fig2.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
