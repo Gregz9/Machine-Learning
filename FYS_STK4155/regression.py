ecca57ec4175f6e50f72e96b1fb17557f4f85aaf
Gregz9 <gregorkajda@gmail.com>
2022-09-28 16:43:24 +0200

    Finished OSL, restructured code

--- FYS_STK4155/regression.py
+++ FYS_STK4155/regression.py
@@ -39,6 +39,9 @@ def generate_design_matrix(x, y, order, intercept=True):
             X = np.column_stack((X, x**(i+1), y**(i+1)))
         else: 
             X = np.column_stack((X, x**(i+1), y**(i+1), (x**i)*(y**i)))
+
+    if not intercept: 
+        X = np.delete(X, 0, axis=1)
     return np.array(X) 
 
 def compute_optimal_parameters(A, y, intercept=True): 
@@ -63,22 +66,18 @@ def compute_optimal_parameters2(X, y):
 
 def optimal_parameters_inv(X, y): 
     beta = np.linalg.inv(X.T.dot(X)).dot(X.T)
-    #beta = beta.T
-
     beta = beta@(y.ravel())
     return beta
 
 
 def predict(X, beta, intercept=0): 
-
     franke_pred = np.array(())
     for i in range(X.shape[0]):
         franke_pred = np.append(franke_pred, np.sum(X[i]*beta) + intercept)
-    
+
     return franke_pred
     
 def perform_manual_regression(x, y, beta): 
-
     pred = beta[0] + beta[1]*x + beta[2]*y + beta[3]*x**2 + beta[4]*y**2 + beta[5]*x*y
     return pred
 
@@ -124,62 +123,79 @@ def create_X(x, y, n ):
 
 	return X
 
-def perform_OLS_regression(): 
+def perform_OLS_regression(n_points=40, n=5): 
 
-    n_points = 40
-    n = 5
     x, y = generate_data(n_points)
-    
-    for i in range(1, n): 
+    z = FrankeFunction(x,y)
+    MSE_train_list = []
+    MSE_test_list = []
+    R2_train_list = []
+    R2_test_list = []
+    betas_list = []
+    intercept_list = []
+    preds = []
+
+    for i in range(1, n+1): 
             
-        X = generate_design_matrix(x, y, n)
-        z = FrankeFunction(x, y) 
-
+        X = generate_design_matrix(x, y, i, intercept=False)
         x_train, x_test, y_train, y_test = train_test_split(X, z.ravel(), test_size=0.2, shuffle=True, random_state=42)
-
-        scaler = MinMaxScaler()
-        scaler.fit(x_train)
-        
         #Centering datasets
         x_train_mean = np.average(x_train, axis=0) 
         y_train_mean = np.average(y_train, axis=0)     
-
-        x_train_scaled2 = x_train - x_train_mean
-        y_train_scaled2 = y_train - y_train_mean
         
-        #Data scaled using SciKit-learn scaler
-        x_train_scaled = scaler.transform(x_train)
-        x_test_scaled = scaler.transform(x_test)
-        # Plot the surface of the function
+        # Using centered values of X and y to compute parameters beta
+        x_train_centered = x_train - x_train_mean
+        y_train_centered = y_train - y_train_mean
+        x_test_centered = x_test - x_train_mean 
 
-        beta_SVD_scaled = compute_optimal_parameters(x_train_scaled2, y_train_scaled2)
-        intercept = np.mean(y_train_mean - x_train_mean @ beta_SVD_scaled)
-        
-        preds2 = predict(X, beta_SVD_scaled)
-        preds2 = preds2 + intercept
-        preds2 = preds2.reshape(n_points, n_points)
+        beta_SVD_cn = compute_optimal_parameters(x_train_centered, y_train_centered)
+        betas_list.append(beta_SVD_cn)
 
-        fig = plt.figure()
-        axs = fig.add_subplot(1, 2, 1, projection='3d')
-        surf = axs.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+        intercept = np.mean(y_train_mean - x_train_mean @ beta_SVD_cn)
+        intercept_list.append(intercept)
 
-        # Customization of z-axis
-        axs.set_zlim(-0.10, 1.40)
-        axs.zaxis.set_major_locator(LinearLocator(10))
-        axs.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-        axs.set_title("Frankes's function")
+        preds_visualization = predict(X, beta_SVD_cn, intercept)
+        preds_visualization = preds_visualization.reshape(n_points, n_points)
+        preds.append(preds_visualization)
+        preds_train = predict(x_train_centered, beta_SVD_cn, intercept) 
+        preds_test = predict(x_test_centered, beta_SVD_cn, intercept)
+        MSE_train_list.append(MSE(y_train, preds_train))
+        MSE_test_list.append(MSE(y_test, preds_test))
+        R2_train_list.append(R2(y_train, preds_train))
+        R2_test_list.append(R2(y_test, preds_test))
 
-        axs = fig.add_subplot(1, 2, 2, projection='3d')
-        surf = axs.plot_surface(x, y, preds2, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        axs.set_zlim(-0.10, 1.40)
-        axs.zaxis.set_major_locator(LinearLocator(10))
-        axs.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-        axs.set_title("Polynomial fit of n-th order to Franke's function")
-        # Add a color bar which maps values to colors 
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-        #fig2.colorbar(surf, shrink=0.5, aspect=5)
-        plt.show()
-    
-perform_OLS_regression()
+    return betas_list, preds, intercept_list, MSE_train_list, MSE_test_list, R2_train_list, R2_test_list
+
+def plot_figs(*args):
+    for element in args: 
+        print('\n')
+        print(element)
+
+
+    """   
+    fig = plt.figure()
+    axs = fig.add_subplot(1, 2, 1, projection='3d')
+    surf = axs.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+    # Customization of z-axis
+    axs.set_zlim(-0.10, 1.40)
+    axs.zaxis.set_major_locator(LinearLocator(10))
+    axs.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    axs.set_title("Frankes's function")
+
+    axs = fig.add_subplot(1, 2, 2, projection='3d')
+    surf = axs.plot_surface(x, y, preds_visualization, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    axs.set_zlim(-0.10, 1.40)
+    axs.zaxis.set_major_locator(LinearLocator(10))
+    axs.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    axs.set_title("Polynomial fit of n-th order")
+    # Add a color bar which maps values to colors 
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    #fig2.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+    """
+
+betas, preds, intercepts, MSE_train, MSE_test, R2_train, R2_test = perform_OLS_regression()
+plot_figs(betas, intercepts)
 
 
