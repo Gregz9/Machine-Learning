381c51595a6e4a03b96ed2ba0a1238f8f90c4680
Gregor Kajda <gregor.kajda@mindshareworld.com>
2022-10-09 08:34:51 +0200

    Further functionality implemented in Regression.py, and reading and plotting of real terrain data tested

--- Applied_Data_Analysis_and_Machine_Learning/Regression.py
+++ Applied_Data_Analysis_and_Machine_Learning/Regression.py
@@ -8,7 +8,7 @@ from sklearn import linear_model
 
 class Reg_model:
 
-    def __init__(self, X=None, z=None, lambas_=[], n_points=0, degree=0, reg_type='ols', resampling_type=None, scaling=None):
+    def __init__(self, X=None, z=None, lambas_=[], n_points=0, degree=0, reg_type='ols', resampling_type=None, scaling=False):
         self.reg_type = reg_type
         self.resampling_type = resampling_type
         self.scaling = scaling
@@ -28,24 +28,25 @@ class Reg_model:
         
     def cost_(self, test_data, pred_data): 
         return MSE(test_data, pred_data)
-        
 
-    def preprocess_data(self, X, z, degree=5, t_size=0.2): 
+
+    def preprocess_data(self, degree=5, t_size=0.2): 
         """
         This method uses the scaling method called centering, which means that we remove first column of the 
         feature matrix in order to remove the intercept term. This is done when we cannot assume that the expected 
         outputs are zero when all predicators are zero. 
         """
+        l = int((degree+1)*(degree+2)/2)
+        
         if self.scaling: 
             self.X = self.X[: , 1:]
-            l = int((degree+1)*(degree+2)/2)
-            self.x_train, self.x_test, self.z_train, self.z_test = train_test_split(self.X[:,:l], z.ravel(), test_size=t_size)
+            self.x_train, self.x_test, self.z_train, self.z_test = train_test_split(self.X[:,:l], self.z.ravel(), test_size=t_size)
 
             self.intercept = np.mean(self.z_train)
             self.x_train -= np.mean(self.x_train)
             self.x_test -= np.mean(self.x_train)
         else: 
-            self.x_train, self.x_test, self.z_train, self.z_test = train_test_split(self.X[:,:l], z.ravel(), test_size=t_size)
+            self.x_train, self.x_test, self.z_train, self.z_test = train_test_split(self.X[:,:l], self.z.ravel(), test_size=t_size)
 
 
     def fit_data(self, X_train, y_train, lambda_=0): 
@@ -54,16 +55,38 @@ class Reg_model:
         elif self.reg_type == 'ridge': 
             self.betas = compute_betas_ridge(X_train,y_train,lambda_)
         elif self.reg_type == 'lasso': 
-            self.RegLasso = linear_model.Lasso(alpha = lambda_, fit_intercept=False, tol=1e-2)
+            self.RegLasso = linear_model.Lasso(alpha = lambda_, fit_intercept=False)
             self.RegLasso.fit(X_train, y_train)
     
-    def predict(self, X_test, betas=0): 
+    def predict(self): 
         if self.reg_type == 'ols' or self.reg_type == 'ridge': 
-            return predict(X_test, betas)
+            if self.scaling: 
+                return (predict(self.x_train, self.betas, self.intercept), 
+                        predict(self.x_test, self.betas, self.intercept))
+            else: 
+                return predict(self.x_train, self.betas), predict(self.x_test, self.betas)
+            
         else: 
-            return self.RegLasso.predict(X_test)
+            if self.scaling:  
+                return (self.RegLasso.predict(self.x_train) + self.intercept, 
+                        self.RegLasso.predict(self.x_test) + self.intercept)
+            else: 
+                return self.RegLasso.predict(self.x_train), self.RegLasso.predict(self.x_test)
+
+
+    def bias_var_tradeOff(self, degree, pred_train, pred_test): 
+        bias = np.mean(np.mean((self.z_train-pred_train)**2, axis=0, keepdims=True))
+        variance = np.mean(np.mean((self.z_test-pred_test)**2, axis=0, keepdims=True))
+        return bias, variance
     
             
+if __name__ == '__main__':
+    reg = Reg_model(n_points=10, degree=11, reg_type='ols')
+
+    print(reg.X.shape) 
+    reg.preprocess_data(degree=5)
+    print(reg.X.shape)
+
 
     
 
--- Applied_Data_Analysis_and_Machine_Learning/test_imread.py
+++ Applied_Data_Analysis_and_Machine_Learning/test_imread.py
@@ -1,31 +1,46 @@
 import numpy as np
-from imageio import imread
+from imageio.v2 import imread
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
 from numpy.random import normal, uniform
 from utils import create_X
+from matplotlib import cm 
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+def visualize_prediction(x,y,z):
+    fig= plt.figure()
+    ax = fig.add_subplot(1, 2, 1,projection='3d')
+    ax.plot_surface(x, y, z, cmap=cm.terrain, linewidth=0, antialiased=False)
+
+    # Customization of z-axis
+    ax.set_zlim(-0.10, 1.75)
+    ax.set_zlabel('Height [km]')
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+   
+    #fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
 
 # Load the terrain
-terrain = imread('C:\\Users\gregor.kajda\OneDrive - insidemedia.net\Desktop\Project_1\Machine-Learning\Applied_Data_Analysis_and_Machine_Learning\Data\SRTM_data_Norway_2.tif')
+terrain = imread('C:\\Users\gregor.kajda\OneDrive - insidemedia.net\Desktop\Project_1\Machine-Learning\Applied_Data_Analysis_and_Machine_Learning\Data\SRTM_data_Norway_1.tif')
 
-N = 2000
+N = 1700
 m = 10 # polynomial order
 terrain = terrain[:N,:N]
+print(terrain.shape)
 # Creates mesh of image pixels
 x = np.linspace(0,1, np.shape(terrain)[0])
 y = np.linspace(0,1, np.shape(terrain)[1])
 x_mesh, y_mesh = np.meshgrid(x,y)
+print(x.shape)
+print(y.shape)
+z = terrain/1000
 
-z = terrain
-X = create_X(x_mesh, y_mesh,m)
+#X = create_X(x, y, z)
+
+visualize_prediction(x_mesh,y_mesh,z.reshape(N,N))
 
 
-# Show the terrain
-plt.figure()
-plt.title('Terrain over Norway 1')
-plt.imshow(terrain, cmap='gray')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
